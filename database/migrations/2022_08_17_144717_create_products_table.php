1680fa8e48870b1f7fc8c0cac466c8c0d22881e6
phong phong <phongtranqb2001@gmail.com>
2022-08-17 22:50:06 +0700

    first commit

---
+++ database/migrations/2022_08_17_144717_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->string('img', 1024);
+            $table->string('description', 1024);
+            $table->float('price')->default(0);
+            $table->integer('quantity')->default(0);
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
